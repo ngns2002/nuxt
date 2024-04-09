5eccb3f25b78904f40d260ba8e0bb5feff1f3f96
Ngo Gia Nhat Son <son.pc01@gmail.com>
2024-04-10 00:20:12 +0700

    update

---
+++ Pages/dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>admin</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NuxtDashboard',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- Pages/login.vue
+++ Pages/login.vue
@@ -1,5 +1,4 @@
 <template>
-   
     <div>
         
         <div class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8 px-6">
