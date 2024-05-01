6e99b41d331fd1d0b4c120a07afca89342810cb2
Ngo Gia Nhat Son <son.pc01@gmail.com>
2024-05-01 21:40:00 +0700

    update

---
+++ Pages/onsite.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h2>1234</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NuxtOnsite',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
